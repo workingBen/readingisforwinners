1f98680231975a095d71c9f0c89f55fbe78f44e8
Ben Pellow <benjamin.pellow@gmail.com>
2014-07-15 23:03:27 -0700

    flash messages :boar:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when "notice" then "alert alert-info"
+      when "warning" then "alert alert-warning"
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-danger"
+      when "alert" then "alert alert-danger"
+    end
+  end
 end
