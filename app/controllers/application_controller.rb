76260774abbd63cdcef4bb3b196954fc4b31d16f
Ben Pellow <benjamin.pellow@gmail.com>
2014-07-18 13:52:43 -0700

    after sign in and sign up paths :bike:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    return "/timeline"
+    return "/reading-list"
+  end
+  def after_sign_up_path_for(resource)
+    return "/reading-list"
   end
 end
